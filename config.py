3da5242ed47efedc242ace94327fc3119e72a527
Kalash-Sheth <shethkalash90@gmail.com>
2026-04-18 17:35:50 +0530

    Read secrets from Streamlit st.secrets with os.environ fallback

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 # config.py
 import os
 
-GROQ_API_KEY      = os.environ.get("GROQ_API_KEY", "")
-VISION_KEY        = os.environ.get("VISION_KEY", "")
-VISION_ENDPOINT   = os.environ.get("VISION_ENDPOINT", "")
-SPEECH_KEY        = os.environ.get("SPEECH_KEY", "")
-SPEECH_REGION     = os.environ.get("SPEECH_REGION", "")
+try:
+    import streamlit as st
+    _secrets = st.secrets
+except Exception:
+    _secrets = {}
+
+def _get(key):
+    if _secrets and key in _secrets:
+        return _secrets[key]
+    return os.environ.get(key, "")
+
+GROQ_API_KEY    = _get("GROQ_API_KEY")
+VISION_KEY      = _get("VISION_KEY")
+VISION_ENDPOINT = _get("VISION_ENDPOINT")
+SPEECH_KEY      = _get("SPEECH_KEY")
+SPEECH_REGION   = _get("SPEECH_REGION")
